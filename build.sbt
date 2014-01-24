595cb1bd0ede060d8d8d7153d81b4c508abbd3c2
sharky <grzegorz.dyk@gmail.com>
2014-01-24 20:05:23 +0100

    Managed to draw something on canvas and use browser console. Yay!

--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
+import sbt.Keys._
+
 // Turn this project into a Scala.js project by importing these settings
 scalaJSSettings
 
@@ -6,7 +8,8 @@ name := "Example"
 version := "0.1-SNAPSHOT"
 
 libraryDependencies ++= Seq(
-    "org.scala-lang.modules.scalajs" %% "scalajs-jasmine-test-framework" % scalaJSVersion % "test"
+    "org.scala-lang.modules.scalajs" %% "scalajs-jasmine-test-framework" % scalaJSVersion % "test",
+    "org.scala-lang.modules.scalajs" %% "scalajs-dom" % "0.1-SNAPSHOT"
 )
 
 // Specify additional .js file to be passed to package-js and optimize-js
--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -1,13 +1,46 @@
 package example
 
+import org.scalajs.dom
+import org.scalajs.dom._
 import scala.scalajs.js
-import js.Dynamic.{ global => g }
 
 object ScalaJSExample {
+  val paragraph = dom.document.createElement("p").asInstanceOf[HTMLParagraphElement]
+
+  val console = dom.window.console
+
   def main(): Unit = {
-    val paragraph = g.document.createElement("p")
     paragraph.innerHTML = "<strong>It works!</strong>"
-    g.document.getElementById("playground").appendChild(paragraph)
+    val container: dom.HTMLDivElement = dom.document.getElementById("playground").asInstanceOf[HTMLDivElement]
+    val canvas = dom.document.createElement("canvas").asInstanceOf[HTMLCanvasElement]
+    canvas.width = 100
+    canvas.height = 100
+    container.appendChild(paragraph)
+    container.appendChild(canvas)
+
+
+    val ctx = canvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
+
+    drawSomething(ctx)
+
+    canvas.onclick = clickHandler
+  }
+
+
+  private def drawSomething(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath()
+    ctx.lineWidth = 10
+    ctx.strokeStyle = "#00ff00"
+    ctx.moveTo(0, 0)
+    ctx.lineTo(100, 100)
+    ctx.stroke()
+  }
+
+  type MouseHandler  = ((MouseEvent) => js.Any)
+
+  private val clickHandler: MouseHandler = {(e: MouseEvent) =>
+    console.log("Works!")
+    console.info(e)
   }
 
   /** Computes the square of an integer.
